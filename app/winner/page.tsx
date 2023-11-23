6f562aaa72f909f75e556eeffd483128644051c0
Mattéo ROSSI <designbymatrossi@gmail.com>
2023-11-23 22:09:58 +0100

    fix quote

--- app/winner/page.tsx
+++ app/winner/page.tsx
@@ -9,7 +9,7 @@ function Winner() {
             <div className="h-screen w-full flex items-center flex-col justify-center gap-5 p-5 relative">
                 <h1 className="text-black text-3xl">Congratulations !!</h1>
                 <h2 className="text-black text-2xl text-center">You win !</h2>
-                <p className="text-black text-xl text-center">You've won absolutely nothing except our congratulations !!</p>
+                <p className="text-black text-xl text-center">{"You've won absolutely nothing except our congratulations !!"}</p>
                 <Lottie animationData={win} className="w-3/4" />
                 <Lottie animationData={firework} className="w-3/4 absolute bottom-0" />
             </div>
